db56acb936e59b9086becfcc42f3e5ef624bcfc3
chankyu11 <lck1130@naver.com>
2020-10-05 16:22:54 +0900

    42번

--- 42.py
+++ 42.py
@@ -1,24 +1,19 @@
-# class Solution:
-#     def trap(self, height: List[int]) -> int:
+class Solution:
+    def trap(self, height: List[int]) -> int:
         
-#         n = len(height)
-#         if n == 0:
-#             return 0
-#         lmax = [0] * n
-#         rmax = [0] * n
-#         lmax[0] = height[0]
-#         rmax[n - 1] = height[n-1]
-#         for i in range(1, n):
-#             lmax[i] = max(lmax[i-1], height[i])
-#         for i in range(n-2, -1, -1):
-#             rmax[i] = max(rmax[i+1], height[i])
+        n = len(height)
+        if n == 0:
+            return 0
+        lmax = [0] * n
+        rmax = [0] * n
+        lmax[0] = height[0]
+        rmax[n - 1] = height[n-1]
+        for i in range(1, n):
+            lmax[i] = max(lmax[i-1], height[i])
+        for i in range(n-2, -1, -1):
+            rmax[i] = max(rmax[i+1], height[i])
 
-#         ans = 0
-#         for i in range(n):
-#             ans += min(lmax[i], rmax[i]) - height[i]
-#         return ans
-h = [0,1,0,2,1,0,1,3,2,1,2,1]
-n = len(h)
-
-for i in range(n - 2 , 0, -1):
-    print(i)
+        ans = 0
+        for i in range(n):
+            ans += min(lmax[i], rmax[i]) - height[i]
+        return ans
