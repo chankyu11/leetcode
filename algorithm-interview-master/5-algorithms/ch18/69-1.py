dfab460ddc0575ea5582db5e7db4d9b0a378f592
CHANKYU\lck11 <lck1130@naver.com>
2020-11-30 15:55:37 +0900

    .

--- algorithm-interview-master/5-algorithms/ch18/69-1.py
+++ algorithm-interview-master/5-algorithms/ch18/69-1.py
@@ -11,10 +11,10 @@ class Solution:
         while row <= len(matrix) - 1 and col >= 0:
             if target == matrix[row][col]:
                 return True
-            # 타겟이 작으면 왼쪽으로
+            # 타겟이 작으면 왼쪽으로 이동
             elif target < matrix[row][col]:
                 col -= 1
-            # 타겟이 크면 아래로
+            # 타겟이 크면 아래로 이동
             elif target > matrix[row][col]:
                 row += 1
         return False
